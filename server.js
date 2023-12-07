56499e32144fb66de99fa5bb448061c43856b650
afnan <afnanballoor44@gmail.com>
2023-12-07 13:05:07 +0530

    created middleware/errorMiddleware.js in backend  2. wrote notfound & errorHandler middlewares abd used both in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5000;
@@ -11,10 +12,13 @@ app.use('/api/users',userRoutes);
 
 app.get('/',(req,res)=>res.send('Server is ready'));
 
+app.use(notFound)
+app.use(errorHandler)
+
 app.listen(port,()=>console.log(`Server started on port ${port}`))
 
 
-
+// 1. created middleware/errorMiddleware.js in backend  2. wrote notfound & errorHandler middlewares abd used both in server.js
 
 
 
