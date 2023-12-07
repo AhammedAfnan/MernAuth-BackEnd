07f8dad9fcfe01fb0eea47480df8fadce17f6012
afnan <afnanballoor44@gmail.com>
2023-12-07 15:31:08 +0530

    1.  sets up mongoUri @.env   2.  created config/db.js inside backend  3. sets up db connection stuffs in db.js  4. calls connectDB in server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import { notFound,errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+const port = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js';
 
-const port = process.env.PORT || 5000;
+connectDB();
 
 const app = express();
 
@@ -17,7 +19,7 @@ app.use(errorHandler)
 
 app.listen(port,()=>console.log(`Server started on port ${port}`))
 
-   
+
 
 
 
