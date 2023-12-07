fb3cb08bfdb35d045c92d5a154fc22e5006f9800
afnan <afnanballoor44@gmail.com>
2023-12-07 11:36:07 +0530

    1. sets up basic route stuffs and wrote a route for /auth  in userRoutes  2. imported userRoutes and wrote a base route for userRoutes in server.js 3. installed and used asynchandler in controllers.

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,12 @@
+import asyncHandler from "express-async-handler";
+
 //   @desc    Auth user/set token
 //   route    POST /api/users/auth
 //   @access  Public
-const authUser = async (req,res) => {
+const authUser = asyncHandler(async (req,res) => {
     res.status(200).json({message:'Auth User'})
-}
+})
 
 export {authUser}
 
 
-// 1. created authUser controller,
